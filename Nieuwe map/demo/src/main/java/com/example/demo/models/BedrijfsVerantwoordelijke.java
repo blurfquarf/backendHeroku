d014497a52a4b5466f35ca59c366b5ce9e63e5f5
blurfquarf <vanlauwemaxou@gmail.com>
2022-03-04 19:13:23 +0100

    bedrijf non-special added

--- Nieuwe map/demo/src/main/java/com/example/demo/models/BedrijfsVerantwoordelijke.java
+++ Nieuwe map/demo/src/main/java/com/example/demo/models/BedrijfsVerantwoordelijke.java
@@ -8,20 +8,26 @@ import javax.persistence.*;
 @Table
 @CrossOrigin(origins = "http://localhost:3000")
 public class BedrijfsVerantwoordelijke extends Person{
+
+    private String bedrijf;
+
+
     public BedrijfsVerantwoordelijke(){
 
     }
 
-    public BedrijfsVerantwoordelijke(String name){
+    public BedrijfsVerantwoordelijke(String name, String bedrijf){
         super(name);
+        this.bedrijf = bedrijf;
     }
 
     public BedrijfsVerantwoordelijke(long id){
         super(id);
     }
 
-    public BedrijfsVerantwoordelijke(long id, String name){
+    public BedrijfsVerantwoordelijke(long id, String name, String bedrijf){
         super(id, name);
+        this.bedrijf = bedrijf;
     }
 
     public long getId() {
@@ -40,11 +46,18 @@ public class BedrijfsVerantwoordelijke extends Person{
         super.setName(name);
     }
 
+    public String getBedrijf() {
+        return  bedrijf;
+    }
+
+
+
+
     @Override
     public String toString() {
         return "Bedrijfsverantwoordelijke{" +
                 "id=" + super.getId() +
-                ", name='" + super.getName() + '\'' +
+                ", name='" + super.getName() + ", bedrijf=" + bedrijf + '\'' +
                 '}';
     }
 
