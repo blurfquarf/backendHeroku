112e90f3810fcbd31dc95e71cc75426f623f9d14
blurfquarf <vanlauwemaxou@gmail.com>
2022-04-09 16:04:03 +0200

    cleared commits

---
+++ Nieuwe map/demo/src/main/java/com/example/demo/config/SubjectConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import com.example.demo.models.Subject;
+import com.example.demo.repository.SubjectRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+@Configuration
+public class SubjectConfig {
+
+
+    @Bean
+    CommandLineRunner commandLineRunner2(SubjectRepository repository){
+        return args -> {
+            Subject taartenBakken = new Subject("Taarten bakken", "sssss");
+
+            Subject nanobots = new Subject("vroemvroem");
+
+            Subject dinos = new Subject("graaaauuww");
+
+
+            repository.saveAll(
+                    List.of(taartenBakken, nanobots, dinos)
+            );
+        };
+    }
+}
---
+++ Nieuwe map/demo/src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,76 @@
+package com.example.demo.controller;
+import java.util.*;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+
+import com.example.demo.models.User;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.security.JwtUtils;
+import com.example.demo.service.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginReq Loginrequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(Loginrequest.getUsername(), Loginrequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody User user) {
+        if (userRepository.existsByUsername(user.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+        if (userRepository.existsByEmail(user.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList(roleRepository.findByName("ROLE_BEDRIJF")));
+        userRepository.save(user);
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+   }
+}
